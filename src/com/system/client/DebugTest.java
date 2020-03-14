19655580207f951f03f3e5cd44b55a96856b4aa6
zerozayu <304307556@qq.com>
2020-03-14 16:41:58 +0800

    eclipse learn

---
+++ src/com/system/client/DebugTest.java
@@ -0,0 +1,11 @@
+package com.system.client;
+
+/**
+ * @author zhangyu
+ * @date 2020/3/14 - 12:42
+ */
+public class DebugTest {
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+    }
+}
